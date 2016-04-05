1a1a8ab79cdeb00bcae3064488127d79c8931089
Matt <mattgruber61@gmail.com>
2016-04-04 20:28:19 -0400

    Added validations to flavor and job

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -2,6 +2,8 @@ class Flavor < ActiveRecord::Base
   has_many :store_flavors
   has_many :stores, through: :store_flavors
 
+  validates_presence_of :name
+
   scope :active,          -> { where(active: true) }
   scope :inactive,        -> { where(active: false) }
   scope :alphabetical,    -> { order('name') }
--- app/models/job.rb
+++ app/models/job.rb
@@ -2,8 +2,10 @@ class Job < ActiveRecord::Base
   has_many :shift_jobs
   has_many :shifts, through: :shift_jobs
 
+  validates_presence_of :name
+
   scope :active,          -> { where(active: true) }
   scope :inactive,        -> { where(active: false) }
   scope :alphabetical,    -> { order('name') }
-  
+
 end
