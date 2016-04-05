29a013bb01dca454b9c956de84e0a750f88b0105
Matt <mattgruber61@gmail.com>
2016-04-04 20:21:37 -0400

    Added scopes to flavor and job

--- app/models/flavor.rb
+++ app/models/flavor.rb
@@ -2,4 +2,8 @@ class Flavor < ActiveRecord::Base
   has_many :store_flavors
   has_many :stores, through: :store_flavors
 
+  scope :active,          -> { where(active: true) }
+  scope :inactive,        -> { where(active: false) }
+  scope :alphabetical,    -> { order('name') }
+
 end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,9 @@
 class Job < ActiveRecord::Base
   has_many :shift_jobs
   has_many :shifts, through: :shift_jobs
+
+  scope :active,          -> { where(active: true) }
+  scope :inactive,        -> { where(active: false) }
+  scope :alphabetical,    -> { order('name') }
+  
 end
